ca380f2fbd3756ec3518ed39bef887bf8e2d79f6
가현 <131218470+bckkingkkang@users.noreply.github.com>
2025-03-08 02:57:01 +0900

    switch문을 이용한 학점 계산

---
+++ Chapter02/SwitchGradeScoreExam/SwitchGradeScoreExam/SwitchGradeScoreExam.cpp
@@ -0,0 +1,30 @@
+﻿#include <cstdio>
+
+void main() {
+	int score;
+	char grade;
+
+	printf("점수를 입력하세요 : ");
+	scanf_s("%d", &score);
+
+	switch (score / 10) {
+	case 10:
+	case 9:
+		grade = 'A';
+		break;
+	case 8 :
+		grade = 'B';
+		break;
+	case 7:
+		grade = 'C';
+		break;
+	case 6 :
+		grade = 'D';
+		break;
+	default:
+		grade = 'F';
+		break;
+	}
+
+	printf("학점은 %c입니다.\n", grade);
+}
