bf846ba91d1092d73791d100acfcf27659518fcd
가현 <131218470+bckkingkkang@users.noreply.github.com>
2025-03-08 02:43:48 +0900

    섭씨-화씨 온도 변환 프로그램

---
+++ Chapter02/CelsToFahr/CelsToFahr/CelsToFahr.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+void main() {
+	int choice;
+	double cels, fahr;
+
+	printf("섭씨에서 화씨 변환 1, 화씨에서 섭씨 변환 2 선택 : ");
+	scanf_s("%d", &choice);
+
+	if (choice == 1) {
+		printf("섭씨에서 화씨로 온도를 변환합니다.\n변환할 섭씨 온도를 입력하세요 : ");
+		scanf_s("%lf", &cels);
+		fahr = 32.0 + (9.0 / 5.0 * cels);
+		printf("변환된 섭씨 온도 %lf의 화씨 온도는 %lf입니다.\n", cels, fahr);
+	}
+	else if(choice == 2) {
+		printf("화씨에서 섭씨로 온도를 변환합니다.\n변환할 화씨 온도를 입력하세요 : ");
+		scanf_s("%lf", &fahr);
+		cels = 5.0 / 9.0 * (fahr - 32.0);
+		printf("변환된 화씨 온도 %lf의 섭씨 온도는 %lf입니다.\n", fahr, cels);
+	}
+	else {
+		printf("유효하지 않은 선택입니다.\n");
+	}
+}
