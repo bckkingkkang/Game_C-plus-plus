7d039831cf3a8b261a4f729e69b5a0bf9a1c398e
가현 <bufar1014@gmail.com>
2025-03-12 05:51:25 +0900

    근로소득세 계산 프로그램 version 3
    배열을 이용한 근로소득세 계산 프로그램

---
+++ Chapter04/IncomeTaxCalcVersion3/IncomeTaxCalcVersion3/IncomeTaxCalcVersion3.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+double CalcTax(int income) {
+	int amount[5] = { 0, 1200, 4600, 8800, 15000 };
+	double tax = 0;
+	double rate[5] = { 0.06, 0.15, 0.24, 0.35, 0.38 };
+
+	for (int i = 0; i < 5; i++) {
+		if (income > amount[i]) {
+			tax += (income - amount[i]) * rate[i];
+			income = amount[i];
+		}
+	}
+	tax += income * rate[0];
+	return tax;
+}
+
+void main() {
+	int income;
+	double tax;
+	printf("연봉을 입력하세요 : ");
+	scanf_s("%d", &income);
+	tax = CalcTax(income);
+	printf("전체 세금은 %f입니다.\n", tax);
+	printf("순수 소득은 %f입니다.\n", income - tax);
+}
