e5634ff5fe4ecae8b227c858cac0209cc5f979ba
가현 <bufar1014@gmail.com>
2025-03-12 06:15:33 +0900

    문자열 처리 함수 사용 예제 코드 추가
    strcpy() : 문자열을 복사하는 함수
    sprintf() : 문자열에 출력하는 함수
    strlen() : 문자열의 길이를 구하는 함수
    strcmp() : 문자열이 같은지 비교하는 함수
    
    책에서는 strcpy, sprinf를 사용 -> C표준 라이브러리에 포함되어 있음, 즉 운영체제나 컴파일러에 관계없이 C 및 C++ 환경에서 동작
    하지만 버퍼 오버플로우 문제 유발 가능
    visual studio에서는 strcpy_s, sprinft_s 사용

---
+++ Chapter04/StringArrayExample/StringArrayExample/StringArrayExample.cpp
@@ -0,0 +1,23 @@
+﻿// 문자열 처리 함수 사용 예
+//#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <string.h>
+
+void main() {
+	char s1[] = "game", s2[20], s3[20];
+
+	strcpy_s(s2, "over");
+	sprintf_s(s3, "%s %s !", s1, s2);
+	// s3 : game over !
+
+	printf("strlen(%s) = %d\n", s1, strlen(s1));
+	printf("strlen(%s) = %d\n", s2, strlen(s2));
+	printf("strlen(%s) = %d\n", s3, strlen(s3));
+
+	// strcmp : 문자열이 같은지 비교하는 함수, 같으면 0을 반환하고 
+	//          s1이 사전적으로 앞에 있으면 -1을, 아니면 1을 반환한다.
+	printf("strcmp(%s,%s) = %d\n", s1, s2, strcmp(s1, s2));
+
+	printf("strcmp(%s, game) = %d\n", s1, strcmp(s1, "game"));
+
+}
