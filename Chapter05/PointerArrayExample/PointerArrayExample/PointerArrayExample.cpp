3e1f65bd15420a8084427b1149edf5e0ff8e1731
가현 <bufar1014@gmail.com>
2025-03-17 23:26:03 +0900

    포인터와 배열의 관계 예제 코드 추가
    배열 이름(배열의 주소, 첫 번째 인덱스)을 포인터에 복사할 수도 있고, 포인터에 항목 추출 연산자 [ ]를 적용할 수도 있다.

---
+++ Chapter05/PointerArrayExample/PointerArrayExample/PointerArrayExample.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+void main() {
+	int list[5] = { 1, 2, 3, 4, 5 };
+	int* p = list;
+	int sum = 0;
+
+	printf("list : ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", *p++);
+	}
+
+	// 포인터에 배열 주소 복사
+	p = list;
+	for (int i = 0; i < 5; i++) {
+		sum += p[i];
+	}
+	printf("\nsum = %d\n", sum);
+}
