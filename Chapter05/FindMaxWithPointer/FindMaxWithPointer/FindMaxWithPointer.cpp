d78b58cb2254fa79b2d15e4ac764a058c7a255cb
가현 <bufar1014@gmail.com>
2025-03-18 00:16:37 +0900

    포인터 매개변수를 이용해 최댓값과 최솟값을 찾는 함수 구현

---
+++ Chapter05/FindMaxWithPointer/FindMaxWithPointer/FindMaxWithPointer.cpp
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+
+void findMinMaxValue(int* a, int len, int* min, int* max) {
+	int maxVal = a[0];
+	
+	if (min != NULL) {
+		*min = a[0];
+		for (int i = 1; i < len; i++) {
+			if (*min > a[i]) {
+				*min = a[i];
+			}
+		}
+	}
+	if (max != NULL) {
+		*max = a[0];
+		for (int i = 1; i < len; i++) {
+			if (*max < a[i]) {
+				*max = a[i];
+			}
+		}
+	}
+}
+
+void main() {
+	int arr[10] = { 3, 24, 82, 12, 34, 7, 53, 17, 26, 51 };
+	int min, max;
+
+	findMinMaxValue(arr, 10, &min, &max);
+	printf("최소 ~ 최대 : %d ~ %d\n", min, max);
+
+}
+/*
+#include <stdio.h>
+
+void findMinMaxValue(int* a, int len, int &min, int &max) {
+	int maxVal = a[0];
+
+	if (min != NULL) {
+		min = a[0];
+		for (int i = 1; i < len; i++) {
+			if (min > a[i]) {
+				min = a[i];
+			}
+		}
+	}
+	if (max != NULL) {
+		max = a[0];
+		for (int i = 1; i < len; i++) {
+			if (max < a[i]) {
+				max = a[i];
+			}
+		}
+	}
+}
+
+void main() {
+	int arr[10] = { 3, 24, 82, 12, 34, 7, 53, 17, 26, 51 };
+	int min, max;
+
+	findMinMaxValue(arr, 10, min, max);
+	printf("최소 ~ 최대 : %d ~ %d\n", min, max);
+}
+*/
