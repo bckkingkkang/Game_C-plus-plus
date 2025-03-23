78499beba7d9b536d968ddfbe2ff3e1009ab38f2
가현 <bufar1014@gmail.com>
2025-03-23 14:48:08 +0900

    지뢰 찾기 구현 파일 작성 중
    실제 지형 정보(Label)와 플레이어에게 보여지는 상태(Mask) 관련 정적 변수, 인라인 함수 작성, 입력받은 좌표가 유효한지 검사하는 bool형 인라인 함수 작성

--- Chapter05/MineSweeper/MineSweeper/MineSweeper.cpp
+++ Chapter05/MineSweeper/MineSweeper/MineSweeper.cpp
@@ -1,1 +1,36 @@
-﻿
+﻿#include "MineSweeper.h"
+
+// Demension(한 변 크기), 게임 판의 기본 크기 정의 DIM * DIM
+#define DIM 9
+
+// Label : 실제 지형 정보
+enum LabelType {
+	Empty = 0,
+	Bomb = 9
+};
+// Mask : 플레이어에게 보여지는 상태
+enum MaskType {
+	Hide = 0,
+	Open,
+	Flag
+};
+
+static int MineMapMask[DIM][DIM];
+static int MineMapLabel[DIM][DIM];
+
+// DIM*DIM을 맵 사이즈의 고정으로 사용할 경우 맵 사이즈 변경 시 불편하기 때문에 nx/ny를 따로 둔다.
+static int nx = DIM, ny = DIM;
+
+// 지뢰의 개수는 사용자 입력을 받지만 9로 미리 초기화
+static int nBomb = 9;
+
+// mask, label 좌표 위치 return하는 인라인 함수 - 단순 반환 뿐만 아니라 값 변경도 가능하게 하기 위해 참조자 사용
+inline int& mask(int x, int y) { return MineMapMask[y][x]; }
+inline int& label(int x, int y) { return MineMapLabel[y][x]; }
+
+// 좌표가 맵 안에 있는지(유효한지) 확인
+inline bool isValid(int x, int y) { return (x >= 0 && x < nx && y >= 0 && y < ny); }
+// 좌표가 맵 안에 있는지 && 해당 좌표에 폭탄이 있는지
+inline bool isBomb(int x, int y) { return isValid(x, y) && label(x, y) == Bomb; }
+// 좌표가 맵 안에 있는지 && 해당 좌표가 비어있는지
+inline bool isEmpty(int x, int y) { return isValid(x, y) && label(x, y) == Empty; }
