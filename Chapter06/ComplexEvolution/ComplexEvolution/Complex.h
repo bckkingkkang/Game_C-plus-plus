1efcca2665aa5452cc40382ca4aa3c836a4bcff2
가현 <bufar1014@gmail.com>
2025-03-29 05:11:52 +0900

    V4 : 모든 멤버 함수의 inline 구현

--- Chapter06/ComplexEvolution/ComplexEvolution/Complex.h
+++ Chapter06/ComplexEvolution/ComplexEvolution/Complex.h
@@ -17,9 +17,24 @@ public:
 	//void addComplex(Complex a, Complex b);
 
 	// 일반 멤버 함수들의 이름을 단순하게 변경, 이 클래스에서만 의미가 있으므로 이름 변경 시에도 의미가 명확함
-	void read(const char* msg = "복소수");
-	void print(const char* msg = "복소수");
-	void add(Complex a, Complex b);
+	//void read(const char* msg = "복소수");
+	//void print(const char* msg = "복소수");
+	//void add(Complex a, Complex b);
+
+	// 모든 멤버 함수를 inline으로 구현, ComplexEvolution.cpp가 필요없음
+	void read(const char* msg = "복소수") {
+		printf("%s", msg);
+		scanf_s("%lf%lf", &real, &imag);
+	}
+	void print(const char* msg = "복소수") {
+		printf("%s %4.2f + %4.2fi\n", msg, real, imag);
+	}
+
+	void add(Complex a, Complex b) {
+		real = a.real + b.real;
+		imag = a.imag + b.imag;
+	}
+
 };
 
 //inline void setcomplex(Complex& c, double r, double i) {
--- Chapter06/ComplexEvolution/ComplexEvolution/ComplexEvolution.cpp
+++ Chapter06/ComplexEvolution/ComplexEvolution/ComplexEvolution.cpp
@@ -3,18 +3,18 @@
 // 모든 멤버 함수에 범위 연산자 :: 적용, 모두 Complex 클래스의 멤버 함수임을 명시
 
 // 반환형이 void로 바뀜, scanf의 인수가 단순해진다.
-void Complex:: read(const char* msg) {
-	printf("%s", msg);
-	scanf_s("%lf%lf", &real, &imag);
-}
-
-
-// 매개변수 줄어듦, 직접 real, imag 사용
-void Complex:: print(const char* msg) {
-	printf("%s %4.2f + %4.2fi\n", msg, real, imag);
-}
-
-void Complex:: add(Complex a, Complex b) {
-	real = a.real + b.real;
-	imag = a.imag + b.imag;
-}
+//void Complex:: read(const char* msg) {
+//	printf("%s", msg);
+//	scanf_s("%lf%lf", &real, &imag);
+//}
+//
+//
+//// 매개변수 줄어듦, 직접 real, imag 사용
+//void Complex:: print(const char* msg) {
+//	printf("%s %4.2f + %4.2fi\n", msg, real, imag);
+//}
+//
+//void Complex:: add(Complex a, Complex b) {
+//	real = a.real + b.real;
+//	imag = a.imag + b.imag;
+//}
