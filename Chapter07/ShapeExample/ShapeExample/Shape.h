8b5d8d0c3511d3964781639d82b237a5bb8f24d2
가현 <bufar1014@gmail.com>
2025-04-04 16:22:28 +0900

    생성자) 멤버 초기화 리스트 : 점과 선분 클래스

--- Chapter07/ShapeExample/ShapeExample/Shape.h
+++ Chapter07/ShapeExample/ShapeExample/Shape.h
@@ -1,3 +1,24 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
+struct Point {
+	int x, y;	// 화면 상의 점을 표현하는 클래스
+	Point(int xx = 0, int yy = 0) : x(xx), y(yy) {}
+	void print() {
+		cout << "\t(" << x << ", " << y << ")\n";
+	}
+};
+
+class Line {	// 화면 상의 선분을 표현하는 클래스
+	Point p1, p2;	// 선분의 양 끝 점
+
+public:
+	Line(int x1, int y1, int x2, int y2) : p1(x1, y1), p2(x2, y2) {}
+
+	void print() {
+		cout << "Line: \n";
+		p1.print();
+		p2.print();
+	}
+};
--- Chapter07/ShapeExample/ShapeExample/ShapeMain.cpp
+++ Chapter07/ShapeExample/ShapeExample/ShapeMain.cpp
@@ -1,1 +1,8 @@
-﻿
+﻿#include "Shape.h"
+
+void main() {
+	Point p(1, 2);
+	Line l(3, 4, 5, 6);
+	p.print();
+	l.print();
+}
