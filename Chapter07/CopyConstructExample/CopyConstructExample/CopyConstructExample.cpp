3b5f35eae55f1c219cece07b723676a5a68e58bf
가현 <bufar1014@gmail.com>
2025-04-06 04:32:25 +0900

    복사 생성자 기본 예제
    대입 연산자가 없어도 객체가 복사되는 상황

---
+++ Chapter07/CopyConstructExample/CopyConstructExample/CopyConstructExample.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Point {
+	int x, y;
+	Point(int xx = 0, int yy = 0) :x(xx), y(yy) { }
+};
+
+Point readPoint() {
+	Point p;
+	cout << "좌표를 입력하세요(x, y) : ";
+	cin >> p.x >> p.y;
+	return p;
+}
+
+void printPoint(Point p, const char* str = "Point") {
+	cout << str << " : (" << p.x << ", " << p.y << ")" << endl;
+}
+
+void main() {
+	Point a;
+	a = readPoint();
+	printPoint(a, "입력 좌표");
+}
