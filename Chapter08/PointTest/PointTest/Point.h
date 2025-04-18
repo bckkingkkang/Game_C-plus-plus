72e3daefa1bd07d119365b145cc4468ffb77792d
가현 <bufar1014@gmail.com>
2025-04-19 02:39:09 +0900

    정적 멤버를 이용한 객체의 수 관리 프로그램 예제 추가

--- Chapter08/PointTest/PointTest/Point.h
+++ Chapter08/PointTest/PointTest/Point.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 class Point {
 	int x;
 	int y;
@@ -7,4 +9,35 @@ public:
 	// 클래스에 정적 멤버 변수 추가
 	static int count;
 	static int version;
+
+	// 정적 멤버 함수
+	static void printCount() {
+		cout << "현재 생성된 객체 Count: " << count << endl;
+	}
+
+	// 생성자
+	Point() {
+		count++;
+	}
+	Point(int x, int y) : x(x), y(y) {
+		count++;
+	}
+	
+	// 소멸자
+	~Point() {
+		count--;
+	}
+
+	Point(const Point& p) : x(p.x), y(p.y) {
+		count++;
+	}
+
+	void add(Point a, Point b) {
+		x = a.x + b.x;
+		y = a.y + b.y;
+	}
+
+	void print(string msg = "P = ") {
+		cout << msg << x << ", " << y << endl;
+	}
 };
--- Chapter08/PointTest/PointTest/PointTest.cpp
+++ Chapter08/PointTest/PointTest/PointTest.cpp
@@ -1,10 +1,31 @@
 ﻿#include "Point.h"
-void main() {
-	Point a;
-	Point::version = 3;
-	Point::count++;
+int main() {
+	//Point a;
+	//Point::version = 3;
+	//Point::count++;
 
 	// 변수 앞에 클래스명과 범위 연산자 :: 를 붙여 정적 멤버 사용
 	// Point::count 는 Point 클래스에 선언된 정적 변수 count를 말한다.
 	// 물론 해당 클래스의 멤버 함수에서는 Point::를 생략할 수 있다. 함수 자체가 그 클래스의 멤버이기 때문
+
+	Point p(1, 2), q(3, 4), r;
+	p.print("p = ");
+	q.print("q = ");
+
+	Point::printCount();
+
+	Point* pPt;
+	pPt = new Point(5, 6);
+	pPt->print("pPt = ");
+
+	Point::printCount();
+	delete pPt;
+
+	Point::printCount();
+
+	r.add(p, q);
+	r.print("P + Q = ");
+
+	Point::printCount();
+
 }
