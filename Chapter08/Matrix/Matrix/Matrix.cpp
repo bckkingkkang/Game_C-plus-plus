f02247cbebe6a6cead3bda08b6fe5be0f9d629c8
가현 <bufar1014@gmail.com>
2025-04-21 03:09:37 +0900

    동적 할당을 이용한 행렬 클래스 예제 추가

---
+++ Chapter08/Matrix/Matrix/Matrix.cpp
@@ -0,0 +1,21 @@
+﻿#include "Matrix.h"
+
+// 2차원 배열에서 가장 큰 값을 찾아 반환하는 함수
+int findMaxFixel(int** a, int rows, int cols) {
+	int max = a[0][0];
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (max < a[i][j]) {
+				max = a[i][j];
+			}
+		}
+	}
+	return max;
+}
+
+void main() {
+	Matrix u(3, 6);
+	u.setRand();
+	u.print("U");
+	cout << "Max Fixel Value : " << findMaxFixel(u.Data(), u.Rows(), u.Cols()) << endl;
+}
---
+++ Chapter08/Matrix/Matrix/Matrix.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Matrix {
+	// 행과 열의 크기
+	int rows, cols;
+	// 실제 행렬 데이터
+	int** mat;
+
+public:
+	// 생성자
+	Matrix(int r = 0, int c = 0) : rows(r), cols(c) {
+		// 디폴트 매개변수 -> 디폴트 생성자 포함
+		mat = new int* [rows];
+		for (int i = 0; i < rows; i++) {
+			mat[i] = new int[cols];
+		}
+	}
+
+	// 소멸자
+	~Matrix() {
+		if (mat != NULL) {
+			for (int i = 0; i < rows; i++) {
+				delete[] mat[i];
+			}
+			delete[] mat;
+		}
+	}
+
+	int Rows() {
+		return rows;
+	}
+	int Cols() {
+		return cols;
+	}
+
+	int& elem(int x, int y) {
+		// 참조 반환, elem(x, y) 호출 시 해당 위치의 요소에 직접 접근 가능
+		return mat[y][x];
+	}
+	
+	// int** mat 반환하는 함수
+	int** Data() {
+		return mat;
+	}
+
+	void print(const char* msg = "Mat") {
+		cout << msg << " " << rows << "x" << cols << endl;
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				cout << setw(4) << mat[i][j];
+			}
+			cout << "\n";
+		}
+	}
+
+	void setRand(int val = 100) {
+		if (mat != NULL) {
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					mat[i][j] = rand() % val;
+				}
+			}
+		}
+	}
+};
